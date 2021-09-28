d62deb492dc3fcf0dcab2fbfe55e8605ed9b8807
Conar Welsh <conarw@gmail.com>
2021-09-27 23:43:23 -0400

    test integration

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Coming Soon</h1>
+      <h1>Coming Soon!!!</h1>
       <img
         src="/coming-soon.gif"
         className={styles.comingSoonImage}
